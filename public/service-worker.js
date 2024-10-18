c655808c6cd266505c0f2d991fb30eecc2b4d2c9
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2024-10-18 09:42:33 +0700

    Fix service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,40 +1,26 @@
-// This is the service worker. Uncomment to run this. Modify based on your needs.
-const CACHE_NAME = 'hwpweb-admin-template-cache-v1';
-const CACHE_URLS = [
-  '/',
-  '/index.php',
-  '/favicon.png', // icons
-  '/Screenshot.png', // Wide Screenshot
+const CACHE_NAME = 'pectk-farmasi-cache-v1';
+const ASSETS_TO_CACHE = [
+  './', 
+  './index.php',
+  './favicon.png',
+  './Screenshot.png'
 ];
 
-// Configure service worker based on your needs
-self.addEventListener('install', (event) => {
+// Install Service Worker dan caching aset
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(CACHE_URLS);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets');
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+// Mengambil aset dari cache
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
