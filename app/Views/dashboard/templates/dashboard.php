1111a3fa9ad473c483b0083632cebe5ad7e22686
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2024-11-29 08:42:41 +0700

    Hide below xl

--- app/Views/dashboard/templates/dashboard.php
+++ app/Views/dashboard/templates/dashboard.php
@@ -284,11 +284,11 @@ $activeSegment = $uri->getSegment(1); // Get the first segment
                     <?= $this->renderSection('title'); ?>
                 </div>
                 <div class="d-flex justify-content-center">
-                    <div class="vr d-none d-lg-block border-success-subtle" style="height: 32px;"></div>
+                    <div class="vr d-none d-xl-block border-success-subtle" style="height: 32px;"></div>
                 </div>
                 <div class="mx-3">
                     <a href="#" class="d-flex align-items-center text-success-emphasis text-decoration-none" data-bs-toggle="offcanvas" data-bs-target="#userOffcanvas" role="button" aria-controls="userOffcanvas">
-                        <div class="me-2 d-none d-lg-block text-end lh-1">
+                        <div class="me-2 d-none d-xl-block text-end lh-1">
                             <span class="text-nowrap" style="font-size: 10pt;"><?= session()->get('fullname') ?></span><br>
                             <span class="text-nowrap" style="font-size: 8pt;">@<?= session()->get('username') ?> • <span class="date"><?= $_SERVER['REMOTE_ADDR'] ?></span></span>
                         </div>
