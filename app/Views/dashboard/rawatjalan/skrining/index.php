8f7ad8f285d3c46f4ec5cb526f6902031e937f89
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-01-17 14:20:48 +0700

    Add pain scale label

--- app/Views/dashboard/rawatjalan/skrining/index.php
+++ app/Views/dashboard/rawatjalan/skrining/index.php
@@ -226,7 +226,20 @@ $usia = $registrasi->diff($tanggal_lahir);
                             </div>
                         </div>
                     </div>
-                    <input type="range" class="form-range" min="0" max="10" id="nyeri_skala" name="nyeri_skala" value="0">
+                    <input type="range" class="form-range" min="0" max="10" id="nyeri_skala" name="nyeri_skala" value="0" list="skala_list">
+                    <datalist id="skala_list" class="d-flex justify-content-between">
+                        <option value="0" label="0"></option>
+                        <option value="1" label="1"></option>
+                        <option value="2" label="2"></option>
+                        <option value="3" label="3"></option>
+                        <option value="4" label="4"></option>
+                        <option value="5" label="5"></option>
+                        <option value="6" label="6"></option>
+                        <option value="7" label="7"></option>
+                        <option value="8" label="8"></option>
+                        <option value="9" label="9"></option>
+                        <option value="10" label="10"></option>
+                    </datalist>
                 </div>
                 <div class="mb-2">
                     <div class="form-floating">
