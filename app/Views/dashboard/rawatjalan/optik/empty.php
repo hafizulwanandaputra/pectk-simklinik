a8107579d6b63d2f356009d1eb76bf4b0466392a
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-01-17 14:07:16 +0700

    Fix active tab on empty eyeglass prescription page

--- app/Views/dashboard/rawatjalan/optik/empty.php
+++ app/Views/dashboard/rawatjalan/optik/empty.php
@@ -66,8 +66,8 @@ $usia = $registrasi->diff($tanggal_lahir);
                             <a class="nav-link py-1 text-nowrap" href="<?= base_url('rawatjalan/penunjang/' . $rawatjalan['id_rawat_jalan']); ?>">Penunjang</a>
                         <?php endif; ?>
                         <?php if (session()->get('role') != 'Perawat') : ?>
-                            <a class="nav-link py-1 text-nowrap active" href="<?= base_url('rawatjalan/resepobat/' . $rawatjalan['id_rawat_jalan']); ?>">Resep Obat</a>
-                            <a class="nav-link py-1 text-nowrap" href="<?= base_url('rawatjalan/optik/' . $rawatjalan['id_rawat_jalan']); ?>">Resep Kacamata</a>
+                            <a class="nav-link py-1 text-nowrap" href="<?= base_url('rawatjalan/resepobat/' . $rawatjalan['id_rawat_jalan']); ?>">Resep Obat</a>
+                            <a class="nav-link py-1 text-nowrap active" href="<?= base_url('rawatjalan/optik/' . $rawatjalan['id_rawat_jalan']); ?>">Resep Kacamata</a>
                         <?php endif; ?>
                         <!-- <a class="nav-link py-1 text-nowrap" href="<?= base_url('rawatjalan/lptindakan/' . $rawatjalan['id_rawat_jalan']); ?>">Laporan Tindakan</a> -->
                     </nav>
