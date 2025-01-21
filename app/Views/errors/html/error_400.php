e19dc0aa42d26040c403ef1ef834646afc224ddb
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-01-21 10:36:05 +0700

    New HTTP 400 page

--- app/Views/errors/html/error_400.php
+++ app/Views/errors/html/error_400.php
@@ -1,84 +1,69 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.78.1">
     <title><?= lang('Errors.badRequest') ?></title>
+    <meta name="theme-color" content="#dc3545">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-        body {
-            height: 100%;
-            background: #fafafa;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            color: #777;
-            font-weight: 300;
-        }
-        h1 {
-            font-weight: lighter;
-            letter-spacing: normal;
-            font-size: 3rem;
-            margin-top: 0;
-            margin-bottom: 0;
-            color: #222;
-        }
-        .wrap {
-            max-width: 1024px;
-            margin: 5rem auto;
-            padding: 2rem;
-            background: #fff;
-            text-align: center;
-            border: 1px solid #efefef;
-            border-radius: 0.5rem;
-            position: relative;
-        }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
-        }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
-        }
-        p {
-            margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
         }
     </style>
-</head>
-<body>
-<div class="wrap">
-    <h1>400</h1>
 
-    <p>
-        <?php if (ENVIRONMENT !== 'production') : ?>
-            <?= nl2br(esc($message)) ?>
-        <?php else : ?>
-            <?= lang('Errors.sorryBadRequest') ?>
-        <?php endif; ?>
-    </p>
-</div>
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/5.3/examples/sign-in/sign-in.css" rel="stylesheet">
+</head>
+
+<body class="d-flex align-items-center py-4 bg-danger text-white ">
+
+    <main class="form-signin w-100 m-auto">
+        <form>
+            <h1 class="display-1 fw-normal">400</h1>
+            <p>
+                <?php if (ENVIRONMENT !== 'production') : ?>
+                    <?= nl2br(esc($message)) ?>
+                <?php else : ?>
+                    <?= lang('Errors.sorryBadRequest') ?>
+                <?php endif; ?>
+            </p>
+            <hr>
+            <small>
+                <p>IP Address: <?= $_SERVER['REMOTE_ADDR']; ?></p>
+                <p>User Agent: <?= $_SERVER['HTTP_USER_AGENT']; ?></p>
+            </small>
+            <hr>
+            <div class="d-grid gap-2 d-flex justify-content-end">
+                <div class="btn-group" role="group">
+                    <button class="btn btn-light btn-block bg-gradient" type="button" onclick="window.history.back();">Go Back</button>
+                </div>
+            </div>
+        </form>
+    </main>
 </body>
+
 </html>
