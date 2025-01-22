e7a9ef20ccaf5ef39f1428dbe017ce86fbe5da25
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-01-22 11:09:40 +0700

    Restored the requirement

--- app/Controllers/Asesmen.php
+++ app/Controllers/Asesmen.php
@@ -303,6 +303,7 @@ class Asesmen extends BaseController
             $validation->setRules([
                 'keluhan_utama' => 'required',
                 'kesadaran' => 'required',
+                'tekanan_darah' => 'required',
                 'nadi' => 'required',
                 'suhu' => 'required',
                 'pernapasan' => 'required',
