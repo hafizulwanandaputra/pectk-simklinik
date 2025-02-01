19b136f151473ff2194137bd2cb390b1d65d2534
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-02-01 11:45:54 +0700

    Remove unwanted hr

--- app/Views/dashboard/pasien/index.php
+++ app/Views/dashboard/pasien/index.php
@@ -93,7 +93,6 @@
                                     </div>
                                 </div>
                             </div>
-                            <hr>
                         </span>
                     <?php endfor; ?>
                 </div>
