3c7560f4454aff3b75778a04a407fba0c2517a6c
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-07-03 11:33:42 +0700

    Reduce blur into 10px

--- app/Views/main-css/index.php
+++ app/Views/main-css/index.php
@@ -388,7 +388,7 @@
     @media (prefers-reduced-transparency: no-preference) {
         .transparent-blur {
             --bs-bg-opacity: 0.75;
-            backdrop-filter: blur(20px);
+            backdrop-filter: blur(10px);
         }
     }
 
