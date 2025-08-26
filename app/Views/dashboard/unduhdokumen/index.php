f58bef8f01b38c596323a9b97cfd67264e6cbbdc
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-08-26 10:16:20 +0700

    Fix download PDF behavior

--- app/Views/dashboard/unduhdokumen/index.php
+++ app/Views/dashboard/unduhdokumen/index.php
@@ -15,7 +15,7 @@
         <ul class="list-group shadow-sm  mb-3">
             <li class="list-group-item p-1 list-group-item-action">
                 <div class="d-flex align-items-start">
-                    <a href="<?= base_url('/unduhdokumen/optik'); ?>" class="stretched-link" style="min-width: 3rem; max-width: 3rem; text-align: center;" target="_blank">
+                    <a href="<?= base_url('/unduhdokumen/optik'); ?>" class="stretched-link download-btn" style="min-width: 3rem; max-width: 3rem; text-align: center;" target="_blank">
                         <p class="mb-0" style="font-size: 1.75rem!important;"><i class="fa-solid fa-glasses"></i></p>
                     </a>
                     <div class="align-self-stretch flex-fill ps-1 text-wrap overflow-hidden d-flex align-items-center" style="text-overflow: ellipsis;">
@@ -36,6 +36,11 @@
 <?= $this->section('javascript'); ?>
 <script>
     $(document).ready(function() {
+        $('.download-btn').on('click', function(e) {
+            e.preventDefault();
+            const url = $(this).attr('href');
+            window.open(url);
+        });
         $('#loadingSpinner').hide();
     });
     // Show toast notification
