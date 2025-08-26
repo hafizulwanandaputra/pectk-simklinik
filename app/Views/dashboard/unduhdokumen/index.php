b854fc4ef83470828b298dee6df241d0cd508de6
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-08-26 11:57:12 +0700

    Add 2 new PDF document ready to download

--- app/Views/dashboard/unduhdokumen/index.php
+++ app/Views/dashboard/unduhdokumen/index.php
@@ -11,8 +11,43 @@
 <?= $this->section('content'); ?>
 <main class="main-content-inside px-3 pt-3">
     <div class="no-fluid-content">
+        <h5>Pasien</h5>
+        <ul class="list-group shadow-sm  mb-3">
+            <li class="list-group-item p-1 list-group-item-action">
+                <div class="d-flex align-items-start">
+                    <a href="<?= base_url('/assets/files/Kartu Berobat PECTK.pdf'); ?>" class="stretched-link download-btn" style="min-width: 3rem; max-width: 3rem; text-align: center;" target="_blank">
+                        <p class="mb-0" style="font-size: 1.75rem!important;"><i class="fa-solid fa-id-card"></i></p>
+                    </a>
+                    <div class="align-self-stretch flex-fill ps-1 text-wrap overflow-hidden d-flex align-items-center" style="text-overflow: ellipsis;">
+                        <div>
+                            <h5 class="card-title">Kartu Berobat</h5>
+                            <span>12 lembar kartu berobat yang siap untuk dicetak dan dipotong. Ini digunakan untuk pasien yang berobat di klinik ini.</span>
+                        </div>
+                    </div>
+                    <div class="align-self-center lh-sm" style="min-width: 3rem; max-width: 3rem; text-align: center;">
+                        <span class="text-body-tertiary"><i class="fa-solid fa-file-arrow-down"></i><br><span style="font-size: 0.75em;">PDF</span></span>
+                    </div>
+                </div>
+            </li>
+        </ul>
         <h5>Rawat Jalan</h5>
         <ul class="list-group shadow-sm  mb-3">
+            <li class="list-group-item p-1 list-group-item-action">
+                <div class="d-flex align-items-start">
+                    <a href="<?= base_url('/assets/files/Resep Obat PECTK.pdf'); ?>" class="stretched-link download-btn" style="min-width: 3rem; max-width: 3rem; text-align: center;" target="_blank">
+                        <p class="mb-0" style="font-size: 1.75rem!important;"><i class="fa-solid fa-file-prescription"></i></p>
+                    </a>
+                    <div class="align-self-stretch flex-fill ps-1 text-wrap overflow-hidden d-flex align-items-center" style="text-overflow: ellipsis;">
+                        <div>
+                            <h5 class="card-title">Resep Obat Luar</h5>
+                            <span>3 lembar resep obat luar yang siap untuk dicetak dan dipotong. Ini digunakan dokter untuk menulis resep obat yang tidak tersedia di klinik ini.</span>
+                        </div>
+                    </div>
+                    <div class="align-self-center lh-sm" style="min-width: 3rem; max-width: 3rem; text-align: center;">
+                        <span class="text-body-tertiary"><i class="fa-solid fa-file-arrow-down"></i><br><span style="font-size: 0.75em;">PDF</span></span>
+                    </div>
+                </div>
+            </li>
             <li class="list-group-item p-1 list-group-item-action">
                 <div class="d-flex align-items-start">
                     <a href="<?= base_url('/unduhdokumen/optik'); ?>" class="stretched-link download-btn" style="min-width: 3rem; max-width: 3rem; text-align: center;" target="_blank">
@@ -21,7 +56,7 @@
                     <div class="align-self-stretch flex-fill ps-1 text-wrap overflow-hidden d-flex align-items-center" style="text-overflow: ellipsis;">
                         <div>
                             <h5 class="card-title">Resep Kacamata Kosong</h5>
-                            <span>Dokumen resep kacamata kosong yang dapat digunakan untuk membuat resep kacamata secara manual.</span>
+                            <span>Dokumen resep kacamata kosong yang siap untuk dicetak. Ini digunakan dokter untuk membuat resep kacamata secara manual.</span>
                         </div>
                     </div>
                     <div class="align-self-center lh-sm" style="min-width: 3rem; max-width: 3rem; text-align: center;">
