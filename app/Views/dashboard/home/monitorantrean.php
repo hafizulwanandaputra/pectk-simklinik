eaa83af483e07cee3317bdc64a4181793b4d2dad
hafizulwanandaputra <wanadaputrahafizul@gmail.com>
2025-08-28 14:16:28 +0700

    Use dvh instead of vh

--- app/Views/dashboard/home/monitorantrean.php
+++ app/Views/dashboard/home/monitorantrean.php
@@ -16,8 +16,8 @@ $db = db_connect();
     }
 
     .full-card-height {
-        max-height: calc((100vh - 3rem - 53px) - 3rem);
-        min-height: calc((100vh - 3rem - 53px) - 3rem);
+        max-height: calc((100dvh - 3rem - 53px) - 3rem);
+        min-height: calc((100dvh - 3rem - 53px) - 3rem);
     }
 
     .main-content-inside {
